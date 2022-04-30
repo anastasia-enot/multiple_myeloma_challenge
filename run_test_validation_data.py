cd26369b6c9b65ea3775bea65d64730ae43821fa
enot <saint.enotishe@gmail.com>
2022-04-30 23:53:29 +0200

    work in progress :construction:

--- run_test_validation_data.py
+++ run_test_validation_data.py
@@ -5,3 +5,7 @@ from preprocessing import preprocess_clinical, preprocess_expression
 
 path_to_clinical_data = ''
 path_to_gene_expression_data = ''
+
+def run_preprocess_and_train(path_to_clinical_data, path_to_gene_expression_data):
+    clin_data = preprocess_clinical(path_to_clinical_data)
+    expression_data = preprocess_expression(path_to_gene_expression_data)
