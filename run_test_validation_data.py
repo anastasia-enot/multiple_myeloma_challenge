6cd92914f3091e9b8014d7f8550fb73876e5be7c
anastasia-whitelab <abarkova@whitelabgx.com>
2022-05-02 16:41:05 +0200

    minor correction :sparkles:

--- run_test_validation_data.py
+++ run_test_validation_data.py
@@ -3,13 +3,12 @@ from multiple_myeloma_challenge.utils.make_predictions_validation_data import ge
 
 # Please modify the paths to the 2 datasets here :
 
-path_to_clinical_data = ''
-path_to_gene_expression_data = ''
+path_to_clinical_data = 'data/sc3_Training_ClinAnnotations.csv'
+path_to_gene_expression_data = 'data/MMRF_CoMMpass_IA9_E74GTF_Salmon_entrezID_TPM_hg19.csv'
 path_to_model = 'trained_models/LogReg_model_BM_80_9_first_visit.sav'
 path_to_scaler = 'trained_models/standard_scaler_BM_80_9_first_visit.sav'
 
 combined_dataset = run_combine_datasets(path_clinical=path_to_clinical_data, path_expression=path_to_gene_expression_data)
-print(combined_dataset)
 get_predictions(path_to_model, path_to_scaler, combined_dataset)
 
 
